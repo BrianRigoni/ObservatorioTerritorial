2a20672cd2543c8753b16eda54b633ea316b7d4e
Brian <brian.rigoni1@gmail.com>
2019-05-15 15:20:07 -0300

    settings - error mio, confundi como se debia escribir el string, ambas funcionan tanto 'directory' como 'directory.app.DirectoryConfig' segui el tuto de mozilla y lo cambie a como estaba antes :P

--- observatorioterritorial/settings.py
+++ observatorioterritorial/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'directory',
+    'directory.apps.DirectoryConfig',
 ]
 
 MIDDLEWARE = [
