548f499e18594f819fcc28d7f2f44e75046493e3
igPerez <ignacioperez583@users.noreply.github.com>
2019-05-18 23:39:17 -0300

    Correciones del Modelo (Investigation, Order)

--- directory/models/project.py
+++ directory/models/project.py
@@ -3,11 +3,11 @@ from .researcher import Researcher
 from .publication import Publication
 
 
-class Investigation(models.Model):
+class Project(models.Model):
     name = models.CharField(max_length= 200, null=False, blank=False)
     description = models.TextField(max_length=1000, null=False, blank=False)
     responsible = models.ForeignKey(Researcher, on_delete=models.DO_NOTHING, related_name='responsability')
-    researchers = models.ManyToManyField(Researcher, related_name='investigation')
+    researchers = models.ManyToManyField(Researcher, through='Authors')
     publications = models.ForeignKey(Publication, on_delete=models.DO_NOTHING)
 
     class Meta:
--- directory/models/publication.py
+++ directory/models/publication.py
@@ -4,7 +4,7 @@ from django.db import models
 class Publication(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     date = models.CharField(max_length=100, null=False, blank=False)
-    link = models.CharField(max_length=200, null=False, blank=False)
+    document = models.FileField(upload_to='directory/publications_files')
 
     class Meta:
         verbose_name = "Publicación"
