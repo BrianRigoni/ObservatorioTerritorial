71c7e4b24cd9ba35eb5b1cdf3b54200f73e4af81
Brian <brian.rigoni1@gmail.com>
2019-06-04 21:00:42 -0300

    importado de clases en el init

--- directory/views/__init__.py
+++ directory/views/__init__.py
@@ -1,6 +1,6 @@
 from .index_view import HomeView
 from .UserView import SignUp, SignIn
 from .ProjectView import ProjectCreateView, ProjectDetailView, ProjectsListView
-from .publication import PublicationsListView, PublicationCreateView
+from .publication_view import PublicationCreateView, PublicationsListView
 
 
