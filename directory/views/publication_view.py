2b6e0719e7f1ffc742235e60d4d44d0d1720fa50
Brian Rigoni <brian.rigoni@hotmail.com>
2019-06-07 21:09:11 -0300

    visualizacion de autores cuando se crea la publicacion

--- directory/views/publication_view.py
+++ directory/views/publication_view.py
@@ -1,5 +1,5 @@
-from django.views.generic import CreateView, ListView
-from directory.models import Publication
+from django.views.generic import CreateView, ListView, View
+from directory.models import Publication, Author, Researcher
 from django.shortcuts import render
 
 
@@ -7,6 +7,11 @@ class PublicationCreateView(CreateView):
     model = Publication
     template_name = 'publications/publication-create.html'
     fields = ['name', 'date', 'genre', 'document', 'project']
+
+    def get(self, request, *args, **kwargs):
+        researchers = Researcher.objects.all()
+        context_dict = {'researchers': researchers}
+        return render(request, self.template_name, context=context_dict)
 
 
 class PublicationsListView(ListView):
@@ -17,3 +22,18 @@ class PublicationsListView(ListView):
         publications = Publication.objects.all()
         context_dict = {'publications': publications}
         return render(request, self.template_name, context=context_dict)
+
+    def post(self, request, *args, **kwargs):
+        pass # TODO sobrescribir el metodo post para crear los registros de los autores con la publicacion
+
+
+class PublicationDownloadView(View):
+    pass 
+    # def download(request, path):
+    # file_path = os.path.join(settings.MEDIA_ROOT, path)
+    # if os.path.exists(file_path):
+    #     with open(file_path, 'rb') as fh:
+    #         response = HttpResponse(fh.read(), content_type="application/vnd.ms-excel")
+    #         response['Content-Disposition'] = 'inline; filename=' + os.path.basename(file_path)
+    #         return response
+    # raise Http404
