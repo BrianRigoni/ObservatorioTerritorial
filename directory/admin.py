2d9e6b341321d7d46619ce769fa01bb0e43366ad
Brian Rigoni <brian.rigoni@hotmail.com>
2019-06-07 21:07:32 -0300

    modificaciones al model + migraciones

--- directory/admin.py
+++ directory/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Institute, Project, Publication, Researcher, University, Authors, Genre
+from .models import Institute, Project, Publication, Researcher, University, Author, Genre
 # Register your models here.
 
 admin.site.register(Institute)
@@ -7,5 +7,5 @@ admin.site.register(Project)
 admin.site.register(Publication)
 admin.site.register(Researcher)
 admin.site.register(University)
-admin.site.register(Authors)
+admin.site.register(Author)
 admin.site.register(Genre)
