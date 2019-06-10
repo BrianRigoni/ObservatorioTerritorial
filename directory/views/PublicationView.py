e2024f62677b7203b9cb885980eb129543dce984
Brian Rigoni <brian.rigoni@hotmail.com>
2019-06-10 19:58:55 -0300

    visualizacion de formulario

--- directory/views/PublicationView.py
+++ directory/views/PublicationView.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from directory.models import Publication
 from django.views.generic import CreateView, ListView, View
-from directory.models import Publication, Author, Researcher
+from directory.models import Publication, Author, Researcher, Genre, Project
 from django.shortcuts import render
 
 
@@ -13,7 +13,9 @@ class PublicationCreateView(LoginRequiredMixin, CreateView):
 
     def get(self, request, *args, **kwargs):
         researchers = Researcher.objects.all()
-        context_dict = {'researchers': researchers}
+        genres = Genre.objects.all()
+        projects = Project.objects.all() # luego filtrar solo por las que participa el usuario
+        context_dict = {'researchers': researchers, 'genres': genres, 'projects': projects}
         return render(request, self.template_name, context=context_dict)
 
 
@@ -26,9 +28,6 @@ class PublicationsListView(LoginRequiredMixin, ListView):
         publications = Publication.objects.all()
         context_dict = {'publications': publications}
         return render(request, self.template_name, context=context_dict)
-
-    def post(self, request, *args, **kwargs):
-        pass # TODO sobrescribir el metodo post para crear los registros de los autores con la publicacion
 
 
 class PublicationDownloadView(View):
