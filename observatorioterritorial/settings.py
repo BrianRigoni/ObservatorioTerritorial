8b92b926beba6c1a7773f1951055bc5e26ca4319
Brian <brian.rigoni1@gmail.com>
2019-06-30 21:54:56 -0300

    updated staticfiles

--- observatorioterritorial/settings.py
+++ observatorioterritorial/settings.py
@@ -128,7 +128,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'directory/media/')
 MEDIA_URL = 'media/'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'directory/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = (
